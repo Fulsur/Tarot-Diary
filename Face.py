d47956ccef4839b589fe41fa1c238d1882998b17
Flower Dreamer <fu2572809217@163.com>
2025-09-17 22:38:18 +0800

    2025.09.17

--- Face.py
+++ Face.py
@@ -2,4 +2,18 @@
 
 import PySide6
 
-class 
+class MainWindow:
+    def __init__(self):
+        self.window = PySide6.QtWidgets.QMainWindow()
+        self.window.setWindowTitle("Face Recognition System")
+        self.window.setGeometry(100, 100, 800, 600)
+
+        # Initialize UI components
+        self.initUI()
+
+    def initUI(self):
+        # Set up the main layout and widgets here
+        pass
+
+    def show(self):
+        self.window.show()
